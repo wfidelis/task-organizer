a5a5d56e797653fc5f1a40f9b7b2091647133c4b
Fidelis Wangui <mainafidelis90@gmail.com>
2020-11-10 14:16:35 +0300

    changed a field to a more appropriate fitting

---
+++ taskapp/taskmanager/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Inbox(models.Model):
+    task = models.CharField(max_length = 255)
+    description = models.TextField(max_length = None)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    time_needed = models.PositiveIntegerField()
+
+
+    
