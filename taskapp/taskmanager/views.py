8b0c07bd082792b72686dfb7ff4dbca9c79fb366
Fidelis Wangui <mainafidelis90@gmail.com>
2020-11-10 16:15:49 +0300

    change views to use generic class-based views

--- taskapp/taskmanager/views.py
+++ taskapp/taskmanager/views.py
@@ -1,51 +1,15 @@
-from rest_framework import status
-from rest_framework.response import Response
 from taskmanager.models import Inbox
 from taskmanager.serializers import InboxSerializer
-from django.http import Http404
-from rest_framework.views import APIView
+from rest_framework import generics
 
-class InboxList(APIView):
-    '''
-    List all tasks in the inbox or create a new task
-    '''
-    def get(self, request, format=None):
-        all_tasks = Inbox.objects.all()
-        serializer = InboxSerializer(all_tasks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = InboxSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class InboxList(generics.ListCreateAPIView):
+        queryset = Inbox.objects.all()
+        serializer_class = InboxSerializer
 
 
-class InboxDetails(APIView):
-    '''
-    get, delete or update an inbox instance
-    '''
-    def get_object(self, pk):
+class InboxDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Inbox.objects.all()
+    serializer_class = InboxSerializer
 
-        try:
-            task = Inbox.objects.get(pk=pk)
-        except Inbox.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, pk, format=None):
-        serializer = InboxSerializer(task)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        data = JSONParser().parse(request)
-        serializer = InboxSerializer(all_tasks, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
