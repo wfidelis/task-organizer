b7cb94208ac9d853c526ef0850f430c526898544
Fidelis Wangui <mainafidelis90@gmail.com>
2020-11-10 14:10:18 +0300

    added url format suffix patterns import relevant modules

--- taskapp/taskmanager/urls.py
+++ taskapp/taskmanager/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from taskmanager import views
 
 urlpatterns = [
     path('tasks/', views.inbox_list, name='all_tasks'),
     path('tasks/<int:pk>/', views.inbox_details, name='task'),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
